5c256124bdbaec36e61baaee1b83bcf6c59d9bad
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-10-17 19:14:15 -0300

    Basic crossover

--- src/evolvepy/generator/selection/selection.py
+++ src/evolvepy/generator/selection/selection.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy.lib.function_base import select
 from numpy.typing import ArrayLike
 from numpy.random import choice
 import numba
---
+++ test/crossover_test.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+
+import numpy as np
+
+sys.path.append("..\src")
+
+import evolvepy.generator.crossover.crossover as crossover
+
+class TestCrossover(unittest.TestCase):
+    crossover_operators = [crossover.mean,  crossover.one_point, crossover.n_point]
+
+    def test_crossover(self):
+
+        chromossomes = np.random.rand(2, 100)
+
+        for operator in TestCrossover.crossover_operators:
+            new_ch = operator(chromossomes)
+
+            self.assertEqual(type(new_ch), np.ndarray) #Correct type
+            self.assertEqual(new_ch.shape, (100,)) #Correct shape
+
+            np.testing.assert_equal(np.any(np.not_equal(new_ch, chromossomes[0])), True)
+            np.testing.assert_equal(np.any(np.not_equal(new_ch, chromossomes[1])), True)
