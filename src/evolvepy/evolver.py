416a3ed9f065d484176f50f2e6097fb4fc24ac1e
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-12-19 13:02:29 -0300

    Revert "first try on elitism"
    This reverts commit e7229209a4d3242f3c5cdc163e8504749d65d6e5.

--- src/evolvepy/evolver.py
+++ src/evolvepy/evolver.py
@@ -6,48 +6,42 @@ from evolvepy.evaluator import Evaluator
 from evolvepy.callbacks import Callback
 
 class Evolver:
-	def __init__(self, generator:Generator, evaluator:Evaluator, generation_size:int, callbacks:Union[Callback, List[Callback]]=None):
-		self._generator = generator
-		self._evaluator = evaluator
-		self._generation_size = generation_size
-		
-		if callbacks is None:
-			callbacks = []
-		elif isinstance(callbacks, Callback):
-			callbacks = [callbacks]
-		self._callbacks = callbacks
+    def __init__(self, generator:Generator, evaluator:Evaluator, generation_size:int, callbacks:Union[Callback, List[Callback]]=None):
+        self._generator = generator
+        self._evaluator = evaluator
+        self._generation_size = generation_size
+        
+        if callbacks is None:
+            callbacks = []
+        elif isinstance(callbacks, Callback):
+            callbacks = [callbacks]
+        self._callbacks = callbacks
 
-		for callback in self._callbacks:
-			callback.generator = generator
-			callback.evaluator = evaluator
+        for callback in self._callbacks:
+            callback.generator = generator
+            callback.evaluator = evaluator
 
-	def evolve(self, generations:int):
+    def evolve(self, generations:int):
 
-		self._history = np.empty((generations, self._generation_size), np.float64)
+        self._history = np.empty((generations, self._generation_size), np.float64)
 
-		for i in range(generations):
-			for callback in self._callbacks:
-				callback.on_generator_start()
+        for i in range(generations):
+            for callback in self._callbacks:
+                callback.on_generator_start()
 
-			population = self._generator.generate(self._generation_size)
+            population = self._generator.generate(self._generation_size)
 
-			random_index = np.random.randint(0, len(population))
-			sub_population1 = population[:random_index]
-			sub_population2 = population[random_index+1:]
-			print(random_index,'\n', oii,'\n', oiii)
-			population = np.concatenate((sub_population1,self._history[i-1].max(),sub_population2))
+            for callback in self._callbacks:
+                callback.on_generator_end()
 
-			for callback in self._callbacks:
-				callback.on_generator_end()
+            fitness = self._evaluator(population)
 
-			fitness = self._evaluator(population)
+            for callback in self._callbacks:
+                callback.on_evaluator_end(fitness)
 
-			for callback in self._callbacks:
-				callback.on_evaluator_end(fitness)
+            self._generator.fitness = fitness
 
-			self._generator.fitness = fitness
-
-			self._history[i] = fitness.flatten()
-		
-		return self._history, population
+            self._history[i] = fitness.flatten()
+        
+        return self._history, population
 
