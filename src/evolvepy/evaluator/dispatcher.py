0494cf44e1eb682dd9819a92b533c2d8477f2578
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2021-12-22 18:58:22 -0300

    Feature/fitness cache (#5)
    Implementado FitnessCache, permitindo armazenar o fitness de indivíduos já avaliados.
    
    - Possibilita definir a quantidade de gerações que serão armazenadas.
    - Permite até quantas casas decimais são consideradas para verificar se dois indivíduos que serão considerados iguais
    - Reestruturado funcionamento dos evaluators
       - Agora cada componente é considerado um EvaluationStage
       - Cada estágio recebe um evaluator ou o próximo estágio
       - Removido o EvaluationManager por desuso
       - Homogeneizado que cada vetor de fitness durante a avaliação possui tamanho (n_indivíduo, n_score), portanto fitness com apenas um score possui tamanho (n, 1) (ao contrário de (n, ) que estava sendo usado anteriormente)

--- src/evolvepy/evaluator/dispatcher.py
+++ src/evolvepy/evaluator/dispatcher.py
@@ -2,25 +2,22 @@ from abc import ABC, abstractmethod
 from typing import Callable
 import numpy as np
 
-from evolvepy.evaluator.evaluator import Evaluator
-
-class EvaluationDispatcher:
-
-    def __call__(self, population:np.ndarray, evaluator:Evaluator) ->np.ndarray:
-        return evaluator(population)
+from evolvepy.evaluator.evaluator import Evaluator, EvaluationStage
 
         
-class MultipleEvaluation(EvaluationDispatcher):
+class MultipleEvaluation(EvaluationStage):
 
-    def __init__(self, n_evaluation:int=1, agregator:Callable[[np.ndarray, int], np.ndarray]=np.mean) -> None:
+    def __init__(self, evaluator:Evaluator, n_evaluation:int=1, agregator:Callable[[np.ndarray, int], np.ndarray]=np.mean) -> None:
+        super().__init__(evaluator)
         self._n_evaluation = n_evaluation
         self._agregator = agregator
+        self._n_scores = 1
 
-    def __call__(self, population: np.ndarray, evaluator: Evaluator) -> np.ndarray:
-        fitness = np.empty((self._n_evaluation, len(population), evaluator._n_scores), dtype=np.float64)
+    def __call__(self, population: np.ndarray) -> np.ndarray:
+        fitness = np.empty((self._n_evaluation, len(population), self._evaluator._n_scores), dtype=np.float64)
 
         for i in range(self._n_evaluation):
-            fitness[i] = evaluator(population)
+            fitness[i] = self._evaluator(population)
 
 
         final_fitness = self._agregator(fitness, axis=0)
