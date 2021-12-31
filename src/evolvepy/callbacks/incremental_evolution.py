92eb9d28ce8d8a719b946d42946f81830f157faf
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-12-31 14:50:10 -0300

    Permitindo um callback de ser bloqueado, a adicionado essa feature ao IncrementalEvolution

--- src/evolvepy/callbacks/incremental_evolution.py
+++ src/evolvepy/callbacks/incremental_evolution.py
@@ -1,14 +1,24 @@
+from typing import Optional, List
+
 from evolvepy.callbacks import Callback
 from evolvepy.generator import Block, FirstGenLayer
 
 class IncrementalEvolution(Callback):
-    def __init__(self, generation_to_start:int, block_layer:Block, first_gen_layer:FirstGenLayer):
-        parameters = {"generation_to_start":generation_to_start, "block_layer_name":block_layer.name, "first_gen_layer_name":first_gen_layer.name}
+    def __init__(self, generation_to_start:int, block_layer:Block, first_gen_layer:FirstGenLayer, callbacks:Optional[List[Callback]]=None):
+        parameters = {"generation_to_start":generation_to_start, "block_layer_name":block_layer.name, "first_gen_layer_name":first_gen_layer.name, "callbacks":[]}
+
+        if callbacks is not None:
+            for callback in callbacks:
+                parameters["callbacks"].append(callback.name)
+        else:
+            callbacks = []
+
         super().__init__(parameters=parameters)
 
         self._generation = 0
         self._block_layer = block_layer
         self._first_gen_layer = first_gen_layer
+        self._callbacks_to_stop = callbacks
 
     def on_generator_start(self) -> None:
         if self._generation == self.parameters["generation_to_start"]:
@@ -18,8 +28,14 @@ class IncrementalEvolution(Callback):
         elif self._generation > self.parameters["generation_to_start"]:
             self._block_layer.parameters["run"] = False
             self._first_gen_layer.parameters["run"] = False
+
+            for callback in self._callbacks:
+                callback.parameters["run"] = True 
         else:
             self._block_layer.parameters["run"] = True
-            self._first_gen_layer.parameters["run"] = False 
+            self._first_gen_layer.parameters["run"] = False
+
+            for callback in self._callbacks_to_stop:
+                callback.parameters["run"] = False 
 
         self._generation += 1
