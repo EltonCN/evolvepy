6b00a1401fbccee2ebc013eeb6c7fd0a40a4899f
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-12-31 11:50:47 -0300

    Criado camada Block
    Camada permite bloquear cromossomos específicos

--- src/evolvepy/generator/__init__.py
+++ src/evolvepy/generator/__init__.py
@@ -1,5 +1,5 @@
 from .layer import Layer, ChromossomeOperator, Concatenate
-from .basic_layers import FilterFirsts, Sort
+from .basic_layers import FilterFirsts, Sort, Block
 from .combine import CombineLayer
 from .generator import Generator
 from .descriptor import Descriptor
