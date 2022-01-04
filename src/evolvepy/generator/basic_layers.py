97104b0826569019c872a155c9698944e6b90184
Joao-Pedro-MB <joaopedrobonucci@gmail.com>
2022-01-03 22:58:52 -0300

    fixed random predation

--- src/evolvepy/generator/basic_layers.py
+++ src/evolvepy/generator/basic_layers.py
@@ -32,20 +32,19 @@ class FilterFirsts(Layer):
 
 class RandomPredation(Layer):
 
-	def __init__(self, n_to_predate:int=1, name: str = None):
+	def __init__(self, n_to_predate:int = 1, name: str = None):
 		parameters = {"n_to_predate":n_to_predate}
 		dynamic_parameters = {"n_to_predate":True}
 		super().__init__(name=name, parameters=parameters, dynamic_parameters=dynamic_parameters)
 
 	def call(self, population: np.ndarray, fitness: np.ndarray, context:Context) -> Tuple[np.ndarray, np.ndarray]:
 		n_to_predate = self.parameters["n_to_predate"]
-		population = population[0:-(n_to_predate)]
-		fitness = fitness[0:-(n_to_predate)]
-		indexes = np.random.choice(np.arange(population.size-1), size=n_to_predate)
+		indexes = np.random.choice(np.arange(population.size - n_to_predate), size=n_to_predate)
 
-		new_individuals = []
+		new_population = []
 		new_fitness = []
+
 		for i in range(n_to_predate):
-			new_population[i] = population[indexes[i]]
-			new_fitness[i] = fitness[indexes[i]]
+			new_population.append(population[indexes[i]])
+			new_fitness.append(fitness[indexes[i]])
 		return new_population, new_fitness
--- tests/test_random_predation.py
+++ tests/test_random_predation.py
@@ -17,11 +17,11 @@ class TestSelection(unittest.TestCase):
 			population = [x for x in range(100)]
 			fitness = np.sort(np.random.uniform(-100, 100, TestSelection.n_individuals))
 			n_selected = 12
-			rp = RP()
+			rp = RP(n_to_predate = n_selected)
 			selected, selected_fitness = rp(population, fitness)
 
 			self.assertEqual(n_selected, len(selected))
 			self.assertEqual(n_selected, len(selected_fitness))
 			
 			for i in range(n_selected):
-				self.assertGreater(selected_fitness[i], fitness[-n_selected])
+				self.assertLessEqual(selected_fitness[i], fitness[-n_selected])
