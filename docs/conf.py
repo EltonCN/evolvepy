3490b55c067d3de67d5dec6c4c817d93175dd517
EltonCN <43186596+EltonCN@users.noreply.github.com>
2022-01-13 15:47:39 -0300

    Versão atualizada

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, EvolvePy Developers.'
 author = 'EltonCN, João Bonucci, Thiago Lacerda'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
