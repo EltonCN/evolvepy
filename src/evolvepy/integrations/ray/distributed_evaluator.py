42129e280187d303f31a62204a866a339413d176
EltonCN <43186596+EltonCN@users.noreply.github.com>
2023-07-02 20:11:19 -0300

    Distributed Evaluator documented

--- src/evolvepy/integrations/ray/distributed_evaluator.py
+++ src/evolvepy/integrations/ray/distributed_evaluator.py
@@ -6,10 +6,11 @@ import numpy as np
 
 from evolvepy.evaluator import Evaluator, ProcessFitnessFunction
 
+
 @ray.remote
 def evaluate_forever(fitness_function: Type[ProcessFitnessFunction], individuals_queue: Queue, scores_queue: Queue, args: Dict[str, object]):
     '''
-    Prepared the cost function to evaluate the individuals, waits for the receipt of individuals and returns the scores.
+    Prepares the cost function to evaluate the individuals, waits for the receipt of individuals, and returns the scores.
 
     Args:
         fitness_function (Type[ProcessFitnessFunction]): Class to be used to evaluate individuals.
@@ -17,7 +18,7 @@ def evaluate_forever(fitness_function: Type[ProcessFitnessFunction], individuals
         scores_queue (Queue): Queue in which the generated scores will be placed.
         args (Dict[str, object]): Other evaluator class constructor arguments.
     '''
-    evaluator =fitness_function(**args)
+    evaluator = fitness_function(**args)
     while True:
         individuals, first, last = individuals_queue.get()
         scores = evaluator(individuals)
@@ -25,13 +26,26 @@ def evaluate_forever(fitness_function: Type[ProcessFitnessFunction], individuals
 
 
 class DistributedEvaluator(Evaluator):
-    def __init__(self, fitness_function: Type[ProcessFitnessFunction], n_worker:int=None, n_scores: int = 1, individual_per_call: int = 1, args:Dict[str, object]=None, name:str=None):
+    def __init__(self, fitness_function: Type[ProcessFitnessFunction], n_worker: int = None, n_scores: int = 1, individual_per_call: int = 1, args: Dict[str, object] = None, name: str = None):
+        '''
+        Initializes the DistributedEvaluator.
+
+        Args:
+            fitness_function (Type[ProcessFitnessFunction]): Class to be used to evaluate individuals.
+            n_worker (int, optional): Number of worker actors to create. Defaults to None.
+            n_scores (int, optional): Number of scores per individual. Defaults to 1.
+            individual_per_call (int, optional): Number of individuals to evaluate per actor call. Defaults to 1.
+            args (Dict[str, object], optional): Other evaluator class constructor arguments. Defaults to None.
+            name (str, optional): Name of the evaluator. Defaults to None.
+        '''
         if n_worker is None:
             n_worker = int(ray.available_resources()["CPU"])
-        
-        other_parameters={"evaluation_function_name":fitness_function.__name__, "n_worker":n_worker}
-        super().__init__(n_scores=n_scores, individual_per_call=individual_per_call, name=name, other_parameters=other_parameters)
-        
+
+        other_parameters = {
+            "evaluation_function_name": fitness_function.__name__, "n_worker": n_worker}
+        super().__init__(n_scores=n_scores, individual_per_call=individual_per_call,
+                         name=name, other_parameters=other_parameters)
+
         self._n_worker = n_worker
         self._fitness_function = fitness_function
         self._individuals_queue = Queue()
@@ -44,19 +58,31 @@ class DistributedEvaluator(Evaluator):
         self._setted = False
 
     def _prepare_actors(self):
+        '''
+        Prepares the actors for evaluation.
+        '''
         if self._setted:
             return
-        
-        self._actors = [evaluate_forever.remote(self._fitness_function, self._individuals_queue, self._scores_queue, self._args) 
+
+        self._actors = [evaluate_forever.remote(self._fitness_function, self._individuals_queue, self._scores_queue, self._args)
                         for _ in range(self._n_worker)]
-        
+
         self._setted = True
 
     def call(self, individuals: np.ndarray) -> np.ndarray:
+        '''
+        Evaluates the individuals using distributed evaluation.
+
+        Args:
+            individuals (np.ndarray): Array of individuals to evaluate.
+
+        Returns:
+            np.ndarray: Array of scores corresponding to the evaluated individuals.
+        '''
         self._prepare_actors()
-        
-        n = individuals.shape[0]//self._individual_per_call
-        
+
+        n = individuals.shape[0] // self._individual_per_call
+
         first = 0
         last = 0
         for i in range(n):
@@ -64,7 +90,8 @@ class DistributedEvaluator(Evaluator):
             self._individuals_queue.put((individuals[first:last], first, last))
             first = last
 
-        scores = np.empty((individuals.shape[0], self._n_scores),  dtype=np.float64)
+        scores = np.empty(
+            (individuals.shape[0], self._n_scores), dtype=np.float64)
         received = 0
 
         while received < n:
