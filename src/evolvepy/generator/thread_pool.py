ee38e387bac5c2e6d5f20c68d3ce4e48769f88c0
EltonCN <43186596+EltonCN@users.noreply.github.com>
2023-07-02 20:24:36 -0300

    ThreadPool documentation

--- src/evolvepy/generator/thread_pool.py
+++ src/evolvepy/generator/thread_pool.py
@@ -8,6 +8,13 @@ import numpy as np
 #job = [Layer, individuals, fitness]
 
 def thread_function(job_queue:Queue, end_queue:Queue):
+    '''
+    Thread function to get and run jobs.
+
+    Args:
+        job_queue (Queue): Queue to receive jobs
+        end_queue (Queue): Queue to mark job as done
+    '''
     while True:
         job : Tuple[object, np.ndarray, np.ndarray, object] = job_queue.get()
 
@@ -19,23 +26,27 @@ def thread_function(job_queue:Queue, end_queue:Queue):
         end_queue.put(job[0].name)
 
 class ThreadPool:
+    '''
+    Manager for threads in a multithreaded generator.
 
-    initialized = False
-    thread_pool : List[threading.Thread] = []
-    job_queue : Optional[Queue] = None
-    end_queue : Optional[Queue] = None
-    n_thread : Optional[int] = None
-    waiting : Optional[Set[str]] = None
-    instance = None
-    serial = False
+    Should not be instantiated, all methods are static.
+    '''
+
+    initialized = False #If it is already initialized
+    thread_pool : List[threading.Thread] = [] #List of all threads
+    job_queue : Optional[Queue] = None #Queue to sending jobs to threads
+    end_queue : Optional[Queue] = None #Queue to receiving finished jobs info
+    n_thread : Optional[int] = None #Number of threads. If not setted is equal to number of cores.
+    waiting : Optional[Set[str]] = None #Set of jobs waiting to get done
+    serial = False #If should run the jobs without threads.
 
     @staticmethod
-    def initialize():
+    def _initialize():
+        '''
+        Initialize the ThreadPool, creating the threads.
+        '''
         if ThreadPool.initialized or ThreadPool.serial:
             return
-
-        if ThreadPool.instance is None:
-            ThreadPool.instance = ThreadPool()
 
         if ThreadPool.n_thread is None:
             ThreadPool.n_thread = mp.cpu_count()
@@ -55,7 +66,10 @@ class ThreadPool:
 
     
     @staticmethod
-    def shutdown():
+    def _shutdown():
+        '''
+        Shutdown the ThreadPool, destroying the threads.
+        '''
         if not ThreadPool.initialized:
             return
 
@@ -74,7 +88,16 @@ class ThreadPool:
 
     @staticmethod
     def add_job(job : Tuple[object, np.ndarray, np.ndarray, object]):
-        ThreadPool.initialize()
+        '''
+        Add a job to be run.
+
+        Args:
+            job (Tuple[Layer, np.ndarray, np.ndarray, Context]): Info needed to the job be runned.
+
+        Raises:
+            ValueError: If job does not have 4 elements.
+        '''
+        ThreadPool._initialize()
 
         if len(job) != 4:
             raise ValueError("Job must have 4 elements, but have {0}".format(len(job)))
@@ -87,6 +110,9 @@ class ThreadPool:
 
     @staticmethod
     def wait_for_end():
+        '''
+        Blocks while waits all jobs to be completed
+        '''
         if not ThreadPool.initialized:
             return
         
